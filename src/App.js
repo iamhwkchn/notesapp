6cd889cb60bbeb2b75997d3e2220f7581c399bdd
Sriram Sivaraman <sriramsivaraman@Srirams-MacBook-Air.local>
2023-07-10 12:27:48 -0400

    notes page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Header from './components/Header.js'
+import Body from './components/Body.js'
+import NotesListPage from './pages/NotesListPage.js'
 
 function App() {
   return (
     <div className="App">
         <p>
-          This is my first react project <code>src/App.js</code>
+          <Header />
+          <NotesListPage />
         </p>
     </div>
   );
