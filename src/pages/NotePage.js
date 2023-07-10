c8c701b6dae6aee197be97b8d8c5c62d0649d55a
Sriram Sivaraman <sriramsivaraman@Srirams-MacBook-Air.local>
2023-07-10 18:12:43 -0400

    use effect imp on note page and note list page

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React , {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
-import notes from '../assets/data'
+//import notes from '../assets/data'
 import {Link} from 'react-router-dom'
 import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
 
 const NotePage = (props) => {
+
     let noteId = useParams()
     console.log(noteId.id)
-    let note = notes.find(note =>
-        note.id === Number(noteId.id))    
+    let [note , setNote] = useState(null)
+
+    // let note = notes.find(note =>
+    //     note.id === Number(noteId.id))    
     // if (!note) {
     //     return <div>Note not found</div>;
     //   }
+
+    useEffect(()=>{
+      getNote()
+    }, [noteId.id])
+
+    let getNote = async()=>{
+        let response = await fetch(`http://localhost:8000/notes/${noteId.id}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
+
   return (
     <div className='note'>
         <div className = 'note-header'>
